910fb071056b3c25bedf2d386a12fe2778abd6b5
Keila Hoover <keila.hoover@gmail.com>
2018-02-17 18:49:42 -0700

    Add clickable palette and pixel grid, add page styling

--- script.js
+++ script.js
@@ -1,70 +1,51 @@
-var pixel = document.querySelector('.pixel');
-pixel.addEventListener('click', function () {
-  pixel.style.backgroundColor = "red";
-  console.log('clicked!')
-});
+// Build grid
+for (var i = 0; i < 100; i++) {
+  let pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  let pixelWrap = document.getElementsByClassName('pixel-wrap')[0];
+  pixelWrap.appendChild(pixel);
+}
 
 
-// Vidoeo Script
-// $(document).ready(function() {
-//
-//     scaleVideoContainer();
-//
-//     initBannerVideoSize('.video-container .poster img');
-//     initBannerVideoSize('.video-container .filter');
-//     initBannerVideoSize('.video-container video');
-//
-//     $(window).on('resize', function() {
-//         scaleVideoContainer();
-//         scaleBannerVideoSize('.video-container .poster img');
-//         scaleBannerVideoSize('.video-container .filter');
-//         scaleBannerVideoSize('.video-container video');
-//     });
-//
+
+// Global Variables
+// var pixelOne = document.querySelector('.pixel-one');
+// var pixelTwo = document.querySelector('.pixel-two');
+// var pixelThree = document.querySelector('.pixel-three');
+// var pixelFour = document.querySelector('.pixel-four')
+var colorOne = document.querySelector('.color-one');
+// var pixel = document.querySelector('.pixel');
+var colorPalette = document.getElementsByClassName('color-palette')[0]
+
+var paintBrush = '';
+// Event Listeners
+// pixel.addEventListener('click', function () {
+//   pixelOne.style.backgroundColor = "red";
+//   console.log('clicked!')
 // });
 //
-// function scaleVideoContainer() {
+// pixelTwo.addEventListener('click', function () {
+//   pixelTwo.style.backgroundColor = "red";
+//   console.log('second pixel!')
+// });
 //
-//     var height = $(window).height() + 5;
-//     var unitHeight = parseInt(height) + 'px';
-//     $('.homepage-hero-module').css('height',unitHeight);
+// pixelThree.addEventListener('click', function () {
+//   pixelThree.style.backgroundColor = "red";
+//   console.log('3rd pixel!')
+// });
 //
-// }
-//
-// function initBannerVideoSize(element){
-//
-//     $(element).each(function(){
-//         $(this).data('height', $(this).height());
-//         $(this).data('width', $(this).width());
-//     });
-//
-//     scaleBannerVideoSize(element);
-//
-// }
-//
-// function scaleBannerVideoSize(element){
-//
-//     var windowWidth = $(window).width(),
-//     windowHeight = $(window).height() + 5,
-//     videoWidth,
-//     videoHeight;
-//
-//     // console.log(windowHeight);
-//
-//     $(element).each(function(){
-//         var videoAspectRatio = $(this).data('height')/$(this).data('width');
-//
-//         $(this).width(windowWidth);
-//
-//         if(windowWidth < 1000){
-//             videoHeight = windowHeight;
-//             videoWidth = videoHeight / videoAspectRatio;
-//             $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-//
-//             $(this).width(videoWidth).height(videoHeight);
-//         }
-//
-//         $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-//
-//     });
-// }
+// pixelFour.addEventListener('click', function () {
+//   pixelFour.style.backgroundColor = "red";
+//   console.log('4th pixel!')
+// });
+
+// Color Pallete Event Listeners
+colorPalette.addEventListener('click', function (event) {
+  paintBrush = event.target.style.backgroundColor;
+  console.log(paintBrush);
+});
+
+let pixelWrap = document.getElementsByClassName('pixel-wrap')[0];
+pixelWrap.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = paintBrush;
+});
